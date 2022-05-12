ab9c54b51340fd8bb6ac2564559a5a1b1d9208e8
Andres Pereira <a07pereira@gmail.com>
2022-05-12 10:51:00 -0400

    nital push

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="./assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <form class="login-email">
+            <p class="login-text" style="font-size: 2rem; font-weight: 800">Login</p>
+        </form>
+    </div>
+    
+</body>
+</html>
