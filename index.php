61673d01df182b183e537380b7b74bcf5639e65a
Andres Pereira <a07pereira@gmail.com>
2022-05-17 12:03:59 -0400

    styling and index file

--- index.php
+++ index.php
@@ -11,6 +11,16 @@
     <div class="container">
         <form class="login-email">
             <p class="login-text" style="font-size: 2rem; font-weight: 800">Login</p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="input-group">
+                <button class="btn">Login</button>
+            </div>
+            <p class="login-register-text">Don't have an account?</p> <a href="reigster.php">Register Here</a>
         </form>
     </div>
     
